16142459a20f368ecdb83fcab45a3f903bd31193
msadmin <mohit.sharma@quest.com>
2025-06-19 06:27:32 +0000

    Add project files.

---
+++ WebApplication1Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics.Metrics;
+using WebApplication1Api.Models;
+
+namespace WebApplication1Api.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
